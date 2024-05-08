e1abed467674c3f338a1cf334c4b991651caac20
KishanGopale <71206363+KishanGopale@users.noreply.github.com>
2024-05-08 15:38:01 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create an order
+        Order order = new Order();
+
+        // Add pizzas to the order
+        order.addPizza(new Pizza("Large", "Thin Crust", new String[]{"Cheese", "Pepperoni"}, 12.99));
+        order.addPizza(new Pizza("Medium", "Pan Crust", new String[]{"Mushrooms", "Olives", "Onions"}, 10.99));
+
+        // Calculate and print the total bill
+        System.out.println("Total Bill: $" + order.calculateTotalBill());
+    }
+}
