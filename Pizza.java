c475eb199f5ab4d57588f67d0c540bc0eebe6681
KishanGopale <71206363+KishanGopale@users.noreply.github.com>
2024-05-08 15:35:10 +0530

    Create Pizza.java

---
+++ Pizza.java
@@ -0,0 +1,18 @@
+
+public class Pizza {
+    private String size;
+    private String crustType;
+    private String[] toppings;
+    private double price;
+
+    // Constructor
+    public Pizza(String size, String crustType, String[] toppings, double price) {
+        this.size = size;
+        this.crustType = crustType;
+        this.toppings = toppings;
+        this.price = price;
+    }
+
+    // Getters and setters
+    // Add methods to calculate price based on size, crust type, and toppings
+}
